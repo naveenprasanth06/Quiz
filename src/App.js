425235a4be13203b936aece129f4fce5693f525e
naveenprasanth06 <naveenprasanth.p2023cse@sece.ac.in>
2025-01-23 15:53:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [img, setImg] = useState("https://api.qrserver.com/v1/create-qr-code/?data=Hello&size=150x150");
+  const [load, setLoad] = useState(false);
+  const [qrData, setQrData] = useState("");
+  const [size, setSize] = useState(150);
+
+  function handleQrGeneration() {
+    setLoad(true);
+
+    try {
+      const url = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrData)}&size=${size}x${size}`;
+      setImg(url);
+    } catch (error) {
+      console.error("Error in generating the QR Code:", error);
+    } finally {
+      setLoad(false)
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="qr-container">
+        <h1>QR CODE GENERATOR</h1>
+        <div className="qr-image-container">
+          {load ? (
+            <p className="loading-text">Please Wait...</p>
+          ) : (
+            <img src={img} alt="QR Code" className="qr-image" />
+          )}
+        </div>
+
+        <div className="input-group">
+          <label className="input-label">Data For QR Code</label>
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Enter data for QR Code"
+            onChange={(e) => setQrData(e.target.value)}
+          />
+        </div>
+
+        <div className="input-group">
+          <label className="input-label">Image Size (px)</label>
+          <input
+            type="number"
+            className="input-field"
+            placeholder="Enter Image Size"
+            value={size}
+            onChange={(e) => setSize(Number(e.target.value) || 150)} // Ensure number conversion
+          />
+        </div>
+
+        <button className="generate-btn" onClick={handleQrGeneration}>
+          Generate QR Code
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
